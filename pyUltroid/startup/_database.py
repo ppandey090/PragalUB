3e69189f3dacae647bcdeaefd5f1ea33dc85b0e0
AkhandaNandTripathi <168406488+PragalX@users.noreply.github.com>
2024-10-08 11:40:35 +0530

    Update _database.py

--- pyUltroid/startup/_database.py
+++ pyUltroid/startup/_database.py
@@ -108,7 +108,7 @@ class _BaseDatabase:
 
 
 class MongoDB(_BaseDatabase):
-    def __init__(self, key, dbname="UltroidDB"):
+    def __init__(self, key, dbname="pyPragalUB"):
         self.dB = MongoClient(key, serverSelectionTimeoutMS=5000)
         self.db = self.dB[dbname]
         super().__init__()
